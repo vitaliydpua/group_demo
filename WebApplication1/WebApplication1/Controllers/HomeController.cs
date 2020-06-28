bc4464df9ce50fb05be8a1e70c67fe3849d43762
Администратор <@16-05>
2020-06-28 14:45:02 +0300

    Added return state to HomeController to 'Index' method at the HomeController

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Hello world");
         }
 
         public ActionResult About()
